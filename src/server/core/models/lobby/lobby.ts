b65c47ebb582f61cc36d51c3b74cf7e4951e7cd9
Jesse Rogers <jesse@jesserogers.net>
2021-06-02 22:48:41 -0700

    Update lobby.ts

--- src/server/core/models/lobby/lobby.ts
+++ src/server/core/models/lobby/lobby.ts
@@ -1,22 +1,40 @@
 import WebSocket from 'ws'
-import { Packet, Randomizer } from '../../../../common/core'
+import { IdGenerator, Packet, Randomizer, ILobby } from '@kuroi/common/core'
 import { PacketHandler } from '../packet-handler/packet-handler'
 
-export class Lobby {
+export class Lobby implements ILobby {
+
+  public static readonly DEFAULT_MAX_CLIENTS: byte = 12
 
   public clients: Map<uint32, WebSocket>
 
   public id: uint16
 
+  public name: string
+
+  public maxClients: byte
+
   public wss: WebSocket.Server
   
   private packetHander = new PacketHandler()
 
-  constructor() {
-    this.id = Randomizer.generateNumericId()
+  private idGenerator = new IdGenerator()
+
+  constructor(_lobby?: ILobby) {
+    this.id = _lobby && _lobby.id || Randomizer.generateNumericId()
+    this.name = _lobby && _lobby.name || ''
+    this.maxClients = _lobby && _lobby.maxClients || Lobby.DEFAULT_MAX_CLIENTS
     this.wss = new WebSocket.Server({ noServer: true })
     this.clients = new Map<uint32, WebSocket>()
     this.connect()
+  }
+
+  public getConfig(): ILobby {
+    return {
+      id: this.id,
+      name: this.name,
+      maxClients: this.maxClients
+    }
   }
 
   public connect(): void {
@@ -26,13 +44,16 @@ export class Lobby {
   }
 
   private handshake(_client: WebSocket): void {
-    const id: byte = Randomizer.generateNumericId()
-    // send client ID back to client
-    const _buffer = new ArrayBuffer(2)
-    const _data = new Uint8Array(_buffer)
-    _data[0] = 0 // 0 will be "welcome message" packet ID for now
-    _data[1] = id // send ID
-    _client.send(_data)
+    if (this.clients.size >= this.maxClients) {
+      return _client.close()
+    }
+    const id: byte = this.idGenerator.generateId()
+    // allocate 2 byte buffer for packet
+    const _buffer = Buffer.alloc(Uint8Array.BYTES_PER_ELEMENT * 2)
+    const _packet = new Packet(_buffer)
+    // write two bytes to packet: packet id and the client's new id
+    _packet.writeBytes([0, id])
+    _client.send(_packet.data())
     this.add(id, _client)
   }
 
@@ -61,7 +82,8 @@ export class Lobby {
   private onMessage(_data: WebSocket.Data): void {
     const _packet = new Packet(_data as Buffer)
     const _packetId: byte = _packet.readByte()
-    const _clientId: byte = _packet.readByte()
+    const _clientId: uint32 = _packet.readByte()
+    console.log(`Received packet from client [${_clientId}]`)
     this.packetHander.handlers[_packetId](_clientId, _packet, this.clients)
   }
 
@@ -70,6 +92,10 @@ export class Lobby {
     console.log(`Removing client [${clientId}]`)
     // remove listeners
     _client.removeAllListeners()
+  }
+
+  public setMaxClients(_max: byte): void {
+    this.maxClients = _max
   }
 
 }
