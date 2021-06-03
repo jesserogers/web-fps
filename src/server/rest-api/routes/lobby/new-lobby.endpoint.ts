45782fdf19490d402fabc41d3ad70c8946546ba1
Jesse Rogers <jesse@jesserogers.net>
2021-06-02 22:55:53 -0700

    update import

--- src/server/rest-api/routes/lobby/new-lobby.endpoint.ts
+++ src/server/rest-api/routes/lobby/new-lobby.endpoint.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express'
 import { ILobby } from '@kuroi/common/core/models'
-import { KuroiLabsAPIEndpoint } from '../../../../common/api'
+import { KuroiLabsAPIEndpoint } from '@kuroi/common/api'
 import { Lobby, LobbyManager } from '../../../core'
 
 function newLobbyHandler(req: Request, res: Response) {
   try {
-    // create new lobby
+    // configure lobby
     const _lobbyConfig: ILobby = { name: 'Shitter Lobby', maxClients: 6 }
     const _lobby = new Lobby(_lobbyConfig)
     LobbyManager.getSharedInstance().add(_lobby)
