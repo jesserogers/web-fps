d7f4450b20bd4b940207b494051e8cedb3f00ac1
jesse <kuro@kuroi.io>
2021-06-03 20:25:14 -0700

    add state to web client

--- src/client/core/services/client.service.ts
+++ src/client/core/services/client.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
 import { ClientPacket } from '@kuroi/common/core/client/net'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class WebClient {
   public static decoder: TextDecoder
 
   public static encoder: TextEncoder
+
+  public static state$ = new BehaviorSubject<byte>(WebSocket.CLOSED)
+
+  private static state: byte = WebSocket.CLOSED
 
   constructor() {
 
@@ -34,6 +39,15 @@ export class WebClient {
     WebClient.socket = _socket
   }
 
+  public static getState(): byte {
+    return WebClient.state
+  }
+
+  public static setState(_state: byte): void {
+    WebClient.state = _state
+    WebClient.state$.next(_state)
+  }
+
   public static getEncoder(): TextEncoder {
     if (!WebClient.encoder) {
       WebClient.encoder = new TextEncoder()
@@ -49,10 +63,11 @@ export class WebClient {
   }
 
   public static send(_packet: ClientPacket): void {
-    if (!WebClient.socket) {
+    if (!WebClient.socket || WebClient.socket.readyState !== WebSocket.OPEN) {
       console.warn('[WebClient.send] no available socket')
       return
     }
+    console.log('Sending packet to server as client [' + WebClient.getId() + ']')
     WebClient.socket.send(_packet.data())
   }
 
