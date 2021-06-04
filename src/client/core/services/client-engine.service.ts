4c7558912e7955063282b8d8988ad348dfaf4e0c
jesse <kuro@kuroi.io>
2021-06-04 00:20:29 -0700

    Update client-engine.service.ts

--- src/client/core/services/client-engine.service.ts
+++ src/client/core/services/client-engine.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { Subject, timer } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { GameObject } from '@kuroi/common/core/models';
+import { Subject } from 'rxjs';
+import { Camera, Scene, WebGLRenderer } from 'three';
 import { ClientNetworkSystem } from '../types';
 import { Destroyer } from '../utils';
 import { AFrameService } from './aframe.service';
@@ -10,26 +11,108 @@ import { AFrameService } from './aframe.service';
 })
 export class ClientEngineService extends Destroyer implements OnDestroy {
 
-  public static TICK_RATE: byte = 30 // tick once per second lul
+  private static _sharedInstance: ClientEngineService
 
-  public static fixedDeltaTime: float = 1000 / ClientEngineService.TICK_RATE
+  public static getSharedinstance(): ClientEngineService {
+    return this._sharedInstance
+  }
 
-  public tick: int = 0
+  public static readonly TICK_RATE: byte = 20
+
+  public static step: float = 1 / ClientEngineService.TICK_RATE
+
+  public static STEP_MS: float = 1000 / ClientEngineService.TICK_RATE
+
+  public static deltaTime: int = 0
+
+  private static lagTime: int = 0
+
+  public static fixedDeltaTime: int = 0
+
+  public lastTick: int
 
   public tick$ = new Subject<int>()
 
+  public renderer: WebGLRenderer
+
+  public lastRenderTimestamp: int
+
+  public scene: Scene
+
+  private _shouldRender: boolean = false
+
+  private _tick: int = 0
+
   constructor(public aframe: AFrameService) {
     super()
+    ClientEngineService._sharedInstance = this
+    this.renderer = new WebGLRenderer()
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
   }
 
-  public start(): void {
-    timer(0, ClientEngineService.fixedDeltaTime).pipe(
-      takeUntil(this._destroyed$)
-    ).subscribe(
-      _tick => {
-        this.tick$.next(_tick)
+  public static getDeltaTime(): float {
+    return ClientEngineService.deltaTime / 1000
+  }
+
+  public static setDeltaTime(_deltaTime: float): void {
+    ClientEngineService.deltaTime = _deltaTime
+  }
+
+  public static getFixedDeltaTime(): float {
+    return ClientEngineService.fixedDeltaTime / 1000
+  }
+
+  public static setFixedDeltaTime(_fixedDeltaTime: float): void {
+    ClientEngineService.fixedDeltaTime = _fixedDeltaTime
+  }
+
+  public stop(): void {
+    this._shouldRender = false
+  }
+
+  public tick(): int {
+    this.lastTick = performance.now()
+    this._tick += 1
+    this.tick$.next(this._tick)
+    return this._tick
+  }
+
+  public run(_scene: Scene, _camera: Camera, _gameObjects: GameObject[]): void {
+    const clientEngine = this
+    this.lastRenderTimestamp = performance.now()
+    this._shouldRender = true
+    // run start hook before gameloop begins
+    _gameObjects.forEach(_object => _object.start());
+    (function _gameLoop(_timestamp: int = 0) {
+      if (!clientEngine._shouldRender) {
+        return
       }
-    )
+      // calculate deltaTime
+      ClientEngineService.setDeltaTime(_timestamp - clientEngine.lastRenderTimestamp)
+      ClientEngineService.lagTime += ClientEngineService.deltaTime
+      // update fixed time step
+      if (ClientEngineService.lagTime >= ClientEngineService.STEP_MS) {
+        // @todo: capture state for networked objects in scene
+        const tick: int = clientEngine.tick()
+        ClientEngineService.setFixedDeltaTime(_timestamp - clientEngine.lastTick)
+        _gameObjects.forEach(_object => {
+          _object.fixedUpdate(tick, ClientEngineService.getFixedDeltaTime())
+        })
+        ClientEngineService.lagTime -= ClientEngineService.STEP_MS
+      }
+      const _interp: float = ClientEngineService.lagTime / ClientEngineService.STEP_MS
+      // run client-side refresh rate update
+      _gameObjects.forEach(_object => {
+        _object.update(_interp)
+      })
+      // render scene
+      clientEngine.renderer.render(_scene, _camera)
+      // save timestamp
+      clientEngine.lastRenderTimestamp = _timestamp
+      // run game loop
+      requestAnimationFrame(_gameLoop)
+    })()
   }
 
   // client engine level get method for networking system
