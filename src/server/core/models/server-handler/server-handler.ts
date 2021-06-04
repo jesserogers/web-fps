11ea33ec8c429a195654e295b05a69499e1ba16c
jesse <kuro@kuroi.io>
2021-06-03 20:28:22 -0700

    update keypress test method to consume user input flags

--- src/server/core/models/server-handler/server-handler.ts
+++ src/server/core/models/server-handler/server-handler.ts
@@ -7,21 +7,20 @@ export class ServerPacketHandler {
 
   public static packets = {
     WELCOME: <byte>0,
-    KEYPRESS: <byte>1
+    USER_INPUT: <byte>1
   }
 
   public handlers: ServerHandler[] = [
     () => undefined,
-    this.keyPressed
+    this.userInputHandler
   ]
 
-  public keyPressed(_clientId: byte, _packet: Packet, _clients: Map<uint32, WebSocket>): void {
-    // get keycode
-    const _keyCode: Uint8Array = _packet.readBytes()
-    const _keyCodeString: string = new TextDecoder().decode(_keyCode)
-    console.log(`Received packet from client [${_clientId}] with key code: "${_keyCodeString}"`)
+  public userInputHandler(_clientId: byte, _packet: Packet, _clients: Map<uint32, WebSocket>): void {
+    // get user inputs
+    const _inputs: uint32 = _packet.readUint32()
+    console.log(`Received packet from client [${_clientId}] with input flags: "${_inputs}"`)
     // send to all other clients
-    _clients.forEach((_client: WebSocket, _id: uint32) => {
+    _clients.forEach((_client: WebSocket, _id: byte) => {
       if (
         // exclude client that sent packet
         _clientId !== _id &&
@@ -29,14 +28,14 @@ export class ServerPacketHandler {
         _client.readyState === WebSocket.OPEN
       ) {
         // create server packet
-        const _byteLength: byte = Uint8Array.BYTES_PER_ELEMENT * 2 + _keyCode.byteLength
+        const _byteLength: byte = Uint8Array.BYTES_PER_ELEMENT * 2 + Uint32Array.BYTES_PER_ELEMENT
         const _out: Packet = new Packet(Buffer.alloc(_byteLength))
         // byte schema: packetId, clientId, keycode
         _out.writeBytes([
-          ServerPacketHandler.packets.KEYPRESS,
-          _clientId,
-          ..._keyCode
+          ServerPacketHandler.packets.USER_INPUT,
+          _clientId
         ])
+        _out.writeUint32(_inputs)
         // send to client
         _client.send(_out.data())
       }
