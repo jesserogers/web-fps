1f45d53056d186c9e2c737ae64f3ecbfb4ce7487
jesse <kuro@kuroi.io>
2021-06-04 00:22:14 -0700

    testing theory on game loop flow
    creating scene from "view" module, in this case the "home" view. once scene is created and configured in the view module, the view module requests the Client Engine to run it with an array of GameObjects on which the engine must execute lifecycle hooks

--- src/client/views/home/home.component.ts
+++ src/client/views/home/home.component.ts
@@ -1,17 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { LobbyService } from '@kuroi/core/services'
+import { CubeBoi } from '@kuroi/game-objects'
+import { ClientEngineService, LobbyService } from '@kuroi/core/services'
 import { take } from 'rxjs/operators'
+import { PerspectiveCamera, Scene } from 'three'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  constructor(private lobbyService: LobbyService, private router: Router) {
+  constructor(
+    private lobbyService: LobbyService,
+    private router: Router,
+    private clientEngine: ClientEngineService
+  ) {
 
+  }
+
+  ngOnInit() {
+    // create scene
+    const scene = new Scene()
+    // create and position camera
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.z = 5;
+    // create game object
+    const cubeBoi = new CubeBoi()
+    // add cube to scene
+    scene.add(cubeBoi.cube);
+    // run scene
+    this.clientEngine.run(scene, camera, [cubeBoi])
   }
 
   public newLobby(): void {
